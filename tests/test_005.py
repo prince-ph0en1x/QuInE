adf7978c4c72de90fa87f1a3ffb8a399b06af531
prince-ph0en1x <aritra44sarkar@yahoo.co.in>
2020-02-12 21:12:33 +0100

    incremental integration

---
+++ tests/test_005.py
@@ -0,0 +1,187 @@
+# Test openql editor, log and button layouts
+
+import sys
+from PyQt5 import QtWidgets, QtGui, QtCore
+
+class Window(QtWidgets.QMainWindow):
+		
+	def __init__(self):
+
+		screen = app.primaryScreen()
+		# print('Screen: %s' % screen.name())
+		# size = screen.size()	# Size of monitor
+		rect = screen.availableGeometry() # Usable size for OS
+		self.win_cx = rect.width()
+		self.win_ry = rect.height()
+
+		super(Window,self).__init__()
+		self.setGeometry(0,0,self.win_cx,self.win_ry)
+
+		self.setWindowTitle("Quantum Innovation Environment")
+		
+		self.setWindowIcon(QtGui.QIcon('icons/app.png'))
+		
+		self.stsmsg = self.statusBar()
+		self.stsmsg.showMessage('Create new project or Open existing project')
+
+		self.saved = 0
+		self.menu()
+		
+		self.show()
+
+	########################## MAIN MENUS ##########################
+
+	def menu(self):
+
+		self.mainMenu = self.menuBar()
+		self.menuFile = self.mainMenu.addMenu('&File')
+		self.fileMenu()
+
+	#~~~~~~~~~~~~~~~~~~~~~~~~~ file menu ~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+	def fileMenu(self):
+
+		self.menuFileNewProj = QtWidgets.QAction("&New Project",self)
+		self.menuFileNewProj.setShortcut("Ctrl+N")
+		self.menuFileNewProj.triggered.connect(self.newProject)
+		self.menuFile.addAction(self.menuFileNewProj)
+
+		self.menuFileOpenProj = QtWidgets.QAction("&Open Project",self)
+		self.menuFileOpenProj.setShortcut("Ctrl+O")
+		self.menuFileOpenProj.triggered.connect(self.openProject)
+		self.menuFile.addAction(self.menuFileOpenProj)
+
+		self.menuFile.addSeparator()
+
+		self.menuFileExit = QtWidgets.QAction("&Exit",self)
+		self.menuFileExit.setShortcut("Ctrl+Q")
+		self.menuFileExit.triggered.connect(self.closeApp)
+		self.menuFile.addAction(self.menuFileExit)
+
+	def closeApp(self):
+		if self.saved == 0:
+			choice = QtWidgets.QMessageBox.question(self,'sanity check',"You have unsaved progress. Are you sure you want to quit?", QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
+			if choice == QtWidgets.QMessageBox.Yes:
+				sys.exit()
+			else:
+				pass
+
+	def newProject(self):
+		self.centerLayout()
+# 
+
+	def openProject(self):
+		return
+
+
+	########################## MAIN LAYOUT ##########################
+
+	def centerLayout(self):
+
+		self.centralWidget = QtWidgets.QWidget(self)
+		self.setCentralWidget(self.centralWidget)
+		self.topLayoutV = QtWidgets.QVBoxLayout(self.centralWidget)
+		
+		self.topLayoutH1 = QtWidgets.QHBoxLayout()
+		self.topLayoutH1.addSpacing(self.win_cx*0.05) # Indent for circuit palate
+		self.openqlEditor()
+		self.qasmEditor()
+		self.topLayoutV.addLayout(self.topLayoutH1)
+		
+		self.topLayoutH2 = QtWidgets.QHBoxLayout()
+		self.resultTabs()     
+		self.topLayoutV.addLayout(self.topLayoutH2)
+
+
+	#~~~~~~~~~~~~~~~~~~~~~~~~~ openql ed ~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+	def openqlEditor(self):
+
+		self.openqlLayout = QtWidgets.QVBoxLayout()
+		
+		self.textOpenql = QtWidgets.QTextEdit(self)
+		self.textOpenql.setFixedSize(self.win_cx*0.55,self.win_ry*0.45)
+		self.textOpenql.setText("OpenQL Editor")
+		self.openqlLayout.addWidget(self.textOpenql)
+
+		self.btnLayoutOC = QtWidgets.QHBoxLayout()
+
+		self.btnO2C = QtWidgets.QPushButton(u"\u2193",self)
+		# self.btnO2C.clicked.connect(self.convO2C)
+		self.btnO2C.setFixedSize(self.win_cx*0.015,self.win_ry*0.03)
+		self.btnLayoutOC.addWidget(self.btnO2C)
+		self.btnO2C.setEnabled(False)
+		
+		self.btnC2O = QtWidgets.QPushButton(u"\u2191",self)
+		# self.btnC2O.clicked.connect(self.convC2O)
+		self.btnC2O.setFixedSize(self.win_cx*0.015,self.win_ry*0.03)
+		self.btnLayoutOC.addWidget(self.btnC2O)
+		self.btnC2O.setEnabled(False)
+		
+		self.openqlLayout.addLayout(self.btnLayoutOC)
+
+		self.topLayoutH1.addLayout(self.openqlLayout)
+
+		self.btnLayoutOQ = QtWidgets.QVBoxLayout()
+		
+		self.btnO2Q = QtWidgets.QPushButton(u"\u2192",self)
+		# self.btnO2Q.clicked.connect(self.convO2Q)
+		self.btnO2Q.setFixedSize(self.win_cx*0.015,self.win_ry*0.03)
+		self.btnLayoutOQ.addWidget(self.btnO2Q)
+
+		self.btnQ2O = QtWidgets.QPushButton(u"\u2190",self)
+		# self.btnQ2O.clicked.connect(self.TBD)
+		self.btnQ2O.setFixedSize(self.win_cx*0.015,self.win_ry*0.03)
+		self.btnLayoutOQ.addWidget(self.btnQ2O)
+		self.btnQ2O.setEnabled(False)
+
+		self.topLayoutH1.addLayout(self.btnLayoutOQ)
+
+	#~~~~~~~~~~~~~~~~~~~~~~~~~ qasm ed ~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+	def qasmEditor(self):
+
+		self.qasmLayout = QtWidgets.QVBoxLayout()
+		
+		self.textQasm = QtWidgets.QTextEdit(self)
+		self.textQasm.setFixedSize(self.win_cx*0.35,self.win_ry*0.45)
+		self.textQasm.setText("QASM Editor")
+		self.textQasm.setLineWrapMode(0)
+		self.qasmLayout.addWidget(self.textQasm)
+
+		self.btnLayoutQ = QtWidgets.QHBoxLayout()
+
+		self.btnQ2C = QtWidgets.QPushButton(u"\u2193",self)
+		# self.btnQ2C.clicked.connect(self.convQ2C)
+		self.btnQ2C.setFixedSize(self.win_cx*0.015,self.win_ry*0.03)
+		self.btnLayoutQ.addWidget(self.btnQ2C,QtCore.Qt.AlignCenter)
+		
+		self.btnC2Q = QtWidgets.QPushButton(u"\u2191",self)
+		# self.btnC2Q.clicked.connect(self.convC2Q)
+		self.btnC2Q.setFixedSize(self.win_cx*0.015,self.win_ry*0.03)
+		self.btnLayoutQ.addWidget(self.btnC2Q,QtCore.Qt.AlignCenter)
+		self.btnC2Q.setEnabled(False)
+
+		self.qasmLayout.addLayout(self.btnLayoutQ)
+
+		self.topLayoutH1.addLayout(self.qasmLayout)
+
+
+	#~~~~~~~~~~~~~~~~~~~~~~~~~ results ~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+	def resultTabs(self):
+
+		self.textLog = QtWidgets.QTextEdit(self)
+		self.textLog.setFixedSize(self.win_cx*0.35,self.win_ry*0.35)
+		self.textLog.setText("Run Logs")
+		self.textLog.setLineWrapMode(0)
+		self.textLog.setFontFamily("Monospace")
+
+		self.tabLayout = QtWidgets.QTabWidget(self)
+		self.tabLayout.addTab(self.textLog,"Logs")
+		
+		self.topLayoutH2.addWidget(self.tabLayout,0,QtCore.Qt.AlignRight)
+
+app = QtWidgets.QApplication(sys.argv)
+GUI = Window()
+sys.exit(app.exec_())
